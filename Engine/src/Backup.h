35f1006ede184a068cce5498cf370f047db66e4a
Ana Alcaraz <alcarazmartinezana@gmail.com>
2026-03-05 19:34:45 +0100

    Add periodic backup cleanup

--- Engine/src/Backup.h
+++ Engine/src/Backup.h
@@ -15,11 +15,17 @@ public:
 private:
 
 	void PerformBackup();
+	void CleanOldBackups();
 	std::string GetTimestamp();
 
 	float timeSinceLastBackup = 0.0f;
-	const float backupInterval = 10.0f; //  (10 rn for testing)
-	//int backupCounter = 0;
+	float timeSinceLastCleanup = 0.0f;
+
+	const float backupInterval = 5.0f; 
+	const float cleanupInterval = 15.0f;  
+	const long long backupMaxAge = 20;    
+	// for testing try: backupInterval = 5.0f, cleanupInterval = 15.0f, backupMaxAge = 20;
+	// also uncomment the log lines in PerformBackup, Start and CleanOldBackups to see the backup process 
 	std::string tempSceneDir;
 };
 

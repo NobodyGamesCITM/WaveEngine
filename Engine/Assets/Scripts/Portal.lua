57d0001d162a25c33ab2aede43236d026d9de3b7
Jowy02 <160216064+Jowy02@users.noreply.github.com>
2026-04-03 17:13:02 +0200

    Script Portal level 2

---
+++ Engine/Assets/Scripts/Portal.lua
@@ -0,0 +1,46 @@
+local NumEstatuas = 0
+local PortalActivado = false
+
+function Start(self)
+    EstatuasPortal = GameObject.FindByTag("EstatuaPortal")
+    estatuasCogidas = {}
+
+    for i, _ in ipairs(EstatuasPortal) do
+        estatuasCogidas[i] = false
+        NumEstatuas = i
+    end
+end
+
+function Update(self, deltaTime)
+    local obj = GameObject.Find("Player")
+    local playerPos = obj.transform.position
+
+    if interact == true then 
+        for i, EstatuaPortal in ipairs(EstatuasPortal) do
+            if not estatuasCogidas[i] then
+                local pos = EstatuaPortal.transform.worldPosition
+                if (math.abs(pos.x - playerPos.x) < 3) then
+                    if (math.abs(pos.z - playerPos.z) < 3) then
+                        Engine.Log("Estatua Portal taken")
+                        NumEstatuas = NumEstatuas - 1
+                        estatuasCogidas[i] = true
+                    end
+                end
+            end
+        end
+    end
+
+    if NumEstatuas == 0 and not PortalActivado then
+        Engine.Log("Activar Portal")
+        PortalActivado = true
+    end
+end 
+
+function OnTriggerEnter(self, other)
+    if other:CompareTag("Player") then
+        if interact and PortalActivado then
+            Engine.Log("Portal Enter")
+        end
+    end
+end
+

b251451fe84c978cb79d5d9a2ce86f086437624e
Ana Alcaraz <alcarazmartinezana@gmail.com>
2026-04-15 22:18:24 +0200

    Update Renderer.cpp

--- Engine/src/Renderer.cpp
+++ Engine/src/Renderer.cpp
@@ -1056,6 +1056,7 @@ bool Renderer::CleanUp()
     if (postProcessShader) postProcessShader->Delete();
 
     meshes.clear();
+    skinnedMeshes.clear();
     activeCameras.clear();
     postProcessingComponents.clear();
     opaqueList.clear();

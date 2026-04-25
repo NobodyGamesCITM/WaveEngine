031b89cde6b2b8fcf5756fecedf4972f7d3642c9
KaiCaire <125651900+KaiCaire@users.noreply.github.com>
2026-04-25 12:20:56 +0200

    Feedback checkpoints (TODO: pulir VFX)

--- Engine/Assets/Scripts/Checkpoints.lua
+++ Engine/Assets/Scripts/Checkpoints.lua
@@ -1,3 +1,6 @@
+--checkpoints script
+
+
 public = {
     --Spawn Offset
     offsetX = 0.0,
@@ -13,32 +16,87 @@ local lastCheckpointPs = nil
 local currentCheckpoint = nil
 local previousCheckpoint = nil
 
-function Start(self)
-    checkpoints = GameObject.FindByTag("CheckPoint")
+local function ActivateParticles(self, checkpoint)
+    if not checkpoint then 
+        Engine.Log("[CHECKPOINT SCRIPT] Checkpoint was nil!")
+        return 
+    end
+
+    lastCheckpointVFX = GameObject.FindInChildren(checkpoint, "LastCheckpointVFX")
+    if lastCheckpointVFX then
+        lastCheckpointVFX:SetActive(true)
+        Engine.Log("Activated Particles GameObject")
+        lastCheckpointPs = lastCheckpointVFX:GetComponent("ParticleSystem")
+        
+        if lastCheckpointPs then 
+            lastCheckpointPs:Play()
+            Engine.Log("Activated CheckPoint Particle System")
+        else 
+            Engine.Log("Couldn't find Particle System on Last Saved CheckPoint VFX GameObject")
+        end
+    else 
+        Engine.Log("Couldn't retrieve Last Saved CheckPoint VFX GameObject")    
+    end
+end
+
+local function StopParticles(self, checkpoint)
+    if not checkpoint then 
+        Engine.Log("[CHECKPOINT SCRIPT] Checkpoint was nil!")
+        return 
+    end
+
+    lastCheckpointVFX = GameObject.FindInChildren(checkpoint, "LastCheckpointVFX")
+    if lastCheckpointVFX then
+        
+        lastCheckpointPs = lastCheckpointVFX:GetComponent("ParticleSystem")
+        if lastCheckpointPs then 
+            lastCheckpointPs:Stop()
+            Engine.Log("Deactivated CheckPoint Particle System")
+        else 
+            Engine.Log("Couldn't find Particle System on Last Saved CheckPoint VFX GameObject")
+        end
+
+        lastCheckpointVFX:SetActive(false)
+        Engine.Log("Deactivated Particles GameObject")
+    else 
+        Engine.Log("Couldn't retrieve Last Saved CheckPoint VFX GameObject")
+    end
+end
+
+local function Initialize(self)
+   checkpoints = GameObject.FindByTag("CheckPoint")
     
     for i, checkpoint in ipairs(checkpoints) do
-        Engine.Log("Deactivating last checkpoint particles form checkpoint ".. i)
+        Engine.Log("Deactivating last checkpoint particles from checkpoint ".. i)
         StopParticles(self, checkpoint)
     end
+end
+
+function Start(self)
+    Initialize(self)
     
 end
 
 function Update(self, deltaTime)
 
+    if not checkpoints then
+        Initialize(self)
+    end
+
     if interact == true then 
-        
+        Engine.Log("Interacted with checkpoint")
         local obj = GameObject.Find("Player")
         local playerPos = obj.transform.position
 
         for i, checkpoint in ipairs(checkpoints) do
-            StopParticles(self, checkPoint)
+            StopParticles(self, checkpoint)
             local pos = checkpoint.transform.worldPosition
             --Engine.Log("Checkpoint x: " ..tostring(pos.x).."  y: "..tostring(pos.y))
             if (math.abs(pos.x - playerPos.x) < self.public.near) then
                 if (math.abs(pos.z - playerPos.z) < self.public.near) then
-                    Engine.Log("Checkpoint taken")
+                    Engine.Log("[CHECKPOINT SCRIPT] Checkpoint taken")
                     previousCheckpoint = currentCheckpoint
-                    currentCheckpoint = checkPoint
+                    currentCheckpoint = checkpoint
                     
                     pos.x = pos.x + self.public.offsetX
                     pos.y = pos.y + self.public.offsetY
@@ -54,36 +112,7 @@ function Update(self, deltaTime)
     end
 end 
 
-function ActivateParticles(self, checkpoint)
-    lastCheckpointVFX = GameObject.FindInChildren(checkpoint, "LastCheckpointVFX")
-    if lastCheckpointVFX then
-        checkPointParticlesGO:SetActive(true)
-        lastCheckpointPs = lastCheckpointVFX:GetComponent("Particle System")
-        
-        if lastCheckpointPs then lastCheckpointPs:Play()
-        else 
-            Engine.Log("Couldn't find Particle System on Last Saved CheckPoint VFX GameObject")
-        end
-    else 
-        Engine.Log("Couldn't retrieve Last Saved CheckPoint VFX GameObject")    
-    end
-end
 
-function StopParticles(self, checkpoint)
-    lastCheckpointVFX = GameObject.FindInChildren(checkpoint, "lastCheckpointVFX")
-    if lastCheckpointVFX then
-        
-        lastCheckpointPs = lastCheckpointVFX:GetComponent("Particle System")
-        if lastCheckpointPs then lastCheckpointPs:Stop()
-        else 
-            Engine.Log("Couldn't find Particle System on Last Saved CheckPoint VFX GameObject")
-        end
-
-        checkPointParticlesGO:SetActive(false)
-    else 
-        Engine.Log("Couldn't retrieve Last Saved CheckPoint VFX GameObject")
-    end
-end
 
 function Restore(self)
     if _G.PotionSystem then

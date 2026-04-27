71dabceb519a463022159df33b493152a43c06be
Bkun <160219482+Bekun67@users.noreply.github.com>
2026-04-27 21:34:41 +0200

    autoskip

--- Engine/Assets/Scripts/SceneChanger.lua
+++ Engine/Assets/Scripts/SceneChanger.lua
@@ -13,11 +13,12 @@ local canvasComponent = nil
 public = {
     targetScene = "Level_02",  
     fadeSpeed   = 1.0,
-    currentLevel = "Level_01"
+    currentLevel = "Level_01",
+    fullIntro = false
 }
 
 function Start(self)
-    if self.public.currentLevel == "Level_01" then _G._PlayerController_introAnim = true end
+    if self.public.currentLevel == "Level_01" and self.public.fullIntro == true and self.gameObject.name == "SceneManager" then _G._PlayerController_introAnim = true end
     currentState = State.FADE_OUT
     currentAlpha = 1.0
     

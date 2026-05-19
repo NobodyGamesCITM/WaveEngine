5e6403aca7921f9fdd364a6d93bdea01ca78198c
Ana Alcaraz <alcarazmartinezana@gmail.com>
2026-05-19 19:31:15 +0200

    Update ModuleResources.cpp

--- Engine/src/ModuleResources.cpp
+++ Engine/src/ModuleResources.cpp
@@ -499,6 +499,7 @@ Resource* ModuleResources::RequestResource(UID uid) {
 
         if (!resource->IsLoadedToMemory()) {
             if (!resource->LoadInMemory()) {
+                LOG_CONSOLE("ERROR: Failed to load resource %llu into memory", uid);
                 return nullptr;
             }
         }
@@ -514,6 +515,7 @@ Resource* ModuleResources::RequestResource(UID uid) {
         return resource;
     }
 
+    LOG_CONSOLE("ERROR: Resource %llu not found", uid);
     return nullptr;
 }
 

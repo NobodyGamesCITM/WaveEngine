f2e22e3f5a93acb05954a8d78f2b5a5cfcf41cc7
KaiCaire <125651900+KaiCaire@users.noreply.github.com>
2026-06-07 04:14:10 +0200

    level2 update

--- Engine/Assets/Scripts/BreakObjectV2.lua
+++ Engine/Assets/Scripts/BreakObjectV2.lua
@@ -1,14 +1,9 @@
---BreakObjectV2 Script (Instantiates Prefab)
-
-
 public = {
     brokenPrefabName = "",
     meshObjName = "",
-    --breakOnTouch = false,
     nearX = 5.0,
     nearY = 5.0,
     nearZ = 5.0,
-    --audioEvent = ""
     dropChance = 0.0,
     dropPrefabName = "HealingPotion",
     cleanUpTime = 5.0,
@@ -26,32 +21,30 @@ local aliveTimer = 0
 
 local myPos 
 local myRot 
-local myScale 
+local myScale
+
+local destroyQueue = {}
+local destroyCooldown = 0
+local DESTROY_INTERVAL = 0.05
 
 
 local function BreakObject(self)
         
     if brokenPrefabPath then 
-        --brokenPrefab:SetActive(true)
         brokenPrefab = Prefab.Instantiate(brokenPrefabPath)
 
         local rand = math.random(0,100)
 
         if rand <= self.public.dropChance then 
-
             Engine.Log("[BreakObject] Random num was "..tostring(rand).." out of "..tostring(self.public.dropChance)..", dropping item...")
             if dropPrefabPath then
                 dropPrefab = Prefab.Instantiate(dropPrefabPath)
             end
-
         else
             Engine.Log("[BreakObject] Random num was "..tostring(rand).." out of "..tostring(self.public.dropChance)..", no drop")
         end
 
-         
         if audioSource then audioSource:PlayAudioEvent() end
-        
-        
 
         myPos = self.transform.worldPosition
         myRot = self.transform.rotation
@@ -60,58 +53,45 @@ local function BreakObject(self)
         if not brokenPrefab then
             --Engine.Log("Unable to load prefab from "..tostring(prefabPath))   
         end
-
     end
 end
 
 local function HideWholeModel(self)
-
     local meshObj = GameObject.FindInChildren(self.gameObject, tostring(self.public.meshObjName))
     if meshObj and meshObj:IsActive() then 
         meshObj:SetActive(false)
         local collider = self.gameObject:GetComponent("Box Collider")
         if collider then collider:Disable() end
-
         hidden = true 
-        --Engine.Log("meshObj deactivated!")
     else
         --Engine.Log("Unable to find meshObj")
     end
-
-    
-    
-
-    --instantiated = true
-
 end
 
 local function Initialize(self)
-    
     brokenPrefabPath = "Prefabs/" ..tostring(self.public.brokenPrefabName)..".prefab"
     dropPrefabPath = "Prefabs/" ..tostring(self.public.dropPrefabName)..".prefab"
-    --Engine.Log("[BreakObject] Prefab Path = "..tostring(prefabPath))
-    
-    --if brokenPrefab then brokenPrefab:SetActive(false) end
     audioSource = self.gameObject:GetComponent("Audio Source")
-
-
-
-    
-
     broken = false
-    --instantiated = false
-    
 end
 
 function Start(self)
     Initialize(self)
-
 end
 
 
 function Update(self, dt)
 
-    
+    -- Process destroy queue
+    if #destroyQueue > 0 then
+        destroyCooldown = destroyCooldown - dt
+        if destroyCooldown <= 0 then
+            local go = table.remove(destroyQueue, 1)
+            if go then GameObject.Destroy(go) end
+            destroyCooldown = DESTROY_INTERVAL
+        end
+    end
+
     if not self.public.breakOnTouch and not broken then
         local playerObj = GameObject.Find("Player")
         local playerPos = playerObj.transform.worldPosition
@@ -124,17 +104,13 @@ function Update(self, dt)
                     if (math.abs(playerPos.z - vasePos.z) <= self.public.nearZ) then
                         BreakObject(self)
                         broken = true
-                        
-                        
                     end
                 end  
             end     
         end
-        
     end
 
     if brokenPrefab then
-        
         if brokenPrefab.transform then 
             brokenPrefab.transform:SetPosition(myPos.x, myPos.y, myPos.z)
             brokenPrefab.transform:SetRotation(myRot.x, myRot.y, myRot.z)
@@ -144,13 +120,11 @@ function Update(self, dt)
                 if dropPrefab.transform then
                     dropPrefab.transform:SetPosition(myPos.x, myPos.y + 2.0, myPos.z)
                     dropPrefab.transform:SetRotation(myRot.x, myRot.y, myRot.z)
-                    --dropPrefab.transform:SetScale(myScale.x, myScale.y, myScale.z)
                 end
             end
 
             if not hidden then HideWholeModel(self) end
         end
-            
     end
 
     if broken then 
@@ -158,25 +132,18 @@ function Update(self, dt)
 
         if aliveTimer >= self.public.cleanUpTime then
             aliveTimer = 0
-            --GameObject.Destroy(brokenPrefab)
-            brokenPrefab.SetActive(false)
+            if brokenPrefab then table.insert(destroyQueue, brokenPrefab) end
+            if dropPrefab then table.insert(destroyQueue, dropPrefab) end
+            brokenPrefab = nil
+            dropPrefab = nil
             self.gameObject:SetActive(false)
-            --GameObject.Destroy(self.gameObject)
-            
         end
     end
 end
 
 function OnCollisionEnter(self, other)
     if other:CompareTag("Enemy") and not broken then 
-        --Engine.Log("[BreakObject] Collided with Enemy")
         BreakObject(self)
         broken = true
     end
 end
-
-
-
-
-
-
